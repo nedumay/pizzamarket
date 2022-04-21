aebdad2bf7c7ea7f1d4da3bf89dbf2a23767a757
Samim <Samim058@gamil.com>
2022-04-21 20:41:53 +0300

    Registration activity

---
+++ app/src/main/java/com/example/pizzamarket/ui/Registration.kt
@@ -0,0 +1,12 @@
+package com.example.pizzamarket.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pizzamarket.R
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
