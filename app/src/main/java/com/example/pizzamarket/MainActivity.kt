8da74ec1d126c2b8a9360eaa3faf6a2549bebcf6
Samim <Samim058@gamil.com>
2022-04-21 20:41:35 +0300

    Registration activity

--- app/src/main/java/com/example/pizzamarket/MainActivity.kt
+++ app/src/main/java/com/example/pizzamarket/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.pizzamarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizzamarket.databinding.ActivityMainBinding
+import com.example.pizzamarket.ui.Registration
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingStart: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        bindingStart = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(bindingStart.root)
+
+        bindingStart.loginBtn.setOnClickListener {
+
+        }
+
+        bindingStart.guestBtn.setOnClickListener {
+            val regIntent = Intent(this@MainActivity, Registration::class.java)
+            startActivity(regIntent)
+        }
+
     }
 }
