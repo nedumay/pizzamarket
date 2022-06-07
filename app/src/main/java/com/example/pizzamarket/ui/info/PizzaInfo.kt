846c48350e20afbd718ada4f9b3598ad2273c0be
Samim <Samim058@gamil.com>
2022-06-07 16:20:08 +0300

    Spinner

--- app/src/main/java/com/example/pizzamarket/ui/info/PizzaInfo.kt
+++ app/src/main/java/com/example/pizzamarket/ui/info/PizzaInfo.kt
@@ -3,39 +3,73 @@ package com.example.pizzamarket.ui.info
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.example.pizzamarket.R
 
 class PizzaInfo : AppCompatActivity() {
 
-    lateinit var size:TextView
-    lateinit var addit:TextView
-    lateinit var deliv:TextView
+    lateinit var size:Spinner
+    lateinit var addit:Spinner
+    lateinit var deliv:Spinner
     lateinit var imgView: ImageView
     lateinit var namePizza: TextView
     lateinit var starText: TextView
     lateinit var priceText: TextView
 
-    //size
-    val size_32 = 1
-    val size_48 = 2
-    val size_55 = 3
-    //additive
-    val no_add = 4
-    val cheese = 5
-    val sauce  = 6
-    //delivery
-    val courier = 7
-    val pickup  = 8
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pizza_info)
 
+        val size_menu = resources.getStringArray(R.array.menu_size)
+        val addit_menu = resources.getStringArray(R.array.addit_menu)
+        val deliver_menu = resources.getStringArray(R.array.menu_deliver)
+
         size = findViewById(R.id.menu_size)
+        if(size !=null){
+            val adapterSize = ArrayAdapter(this,android.R.layout.simple_list_item_1, size_menu)
+            size.adapter = adapterSize
+            size.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, p3: Long) {
+                    Toast.makeText(this@PizzaInfo, getString(R.string.size_pizza ) + " " + size_menu[position], Toast.LENGTH_LONG).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
         addit = findViewById(R.id.menu_additive)
+        if(addit !=null){
+            val adapterAdd = ArrayAdapter(this, android.R.layout.simple_list_item_1, addit_menu)
+            addit.adapter = adapterAdd
+            addit.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, p3: Long) {
+                    Toast.makeText(this@PizzaInfo, getString(R.string.dditives) + " " + addit_menu[position], Toast.LENGTH_LONG).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
         deliv = findViewById(R.id.menu_delivery)
+        if(deliv!=null){
+            val adapterDeliv = ArrayAdapter(this, android.R.layout.simple_list_item_1, deliver_menu)
+            deliv.adapter = adapterDeliv
+            deliv.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, p3: Long) {
+                    Toast.makeText(this@PizzaInfo, getString(R.string.delivery_method) + " " + deliver_menu[position], Toast.LENGTH_LONG).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
+
         imgView = findViewById(R.id.img_view)
         namePizza = findViewById(R.id.name_pizza)
         starText = findViewById(R.id.text_star_pizza)
@@ -50,62 +84,6 @@ class PizzaInfo : AppCompatActivity() {
         namePizza.setText(name!!)
         starText.setText(star)
         priceText.setText(price)
-        //Контекстное меню
-        registerForContextMenu(size)
-        registerForContextMenu(addit)
-        registerForContextMenu(deliv)
-    }
 
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        when (v?.id) {
-            R.id.menu_size -> {
-                menu?.add(0, size_32, 0, R.string.size_32)
-                menu?.add(0, size_48, 0, R.string.size_48)
-                menu?.add(0, size_55, 0, R.string.size_55)
-            }
-            R.id.menu_additive -> {
-                menu?.add(0, no_add, 0, R.string.no)
-                menu?.add(0, cheese, 0, R.string.cheese)
-                menu?.add(0, sauce, 0, R.string.sauce)
-            }
-            R.id.menu_delivery -> {
-                menu?.add(0, courier, 0, R.string.courier)
-                menu?.add(0, pickup, 0, R.string.pickup)
-            }
-        }
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            //Пункты меню для размера
-            size_32 ->{
-                size.setText(R.string.size_32)
-            }
-            size_48 ->{
-                size.setText(R.string.size_48)
-            }
-            size_55 ->{
-                size.setText(R.string.size_55)
-            }
-            //Пункты меню для добавок
-            no_add -> {
-                addit.setText(R.string.no)
-            }
-            cheese -> {
-                addit.setText(R.string.cheese)
-            }
-            sauce -> {
-                addit.setText(R.string.sauce)
-            }
-            //Пункты меню для доставки
-            courier -> {
-                deliv.setText(R.string.courier)
-            }
-            pickup -> {
-                deliv.setText(R.string.pickup)
-            }
-        }
-        return super.onContextItemSelected(item)
     }
 }
